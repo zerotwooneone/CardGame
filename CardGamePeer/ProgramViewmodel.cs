907f8455e7843e2999e099b9fec3c337e5079ddc
David Squire <squiredd@gmail.com>
2019-01-13 03:10:12 -0800

    exit code now works

--- CardGamePeer/ProgramViewmodel.cs
+++ CardGamePeer/ProgramViewmodel.cs
@@ -13,14 +13,14 @@ namespace CardGamePeer
 {
     public class ProgramViewmodel
     {
-        private readonly OutputService _outputService;
-        private readonly IRandomService _randomService;
         private readonly IDeckFactory _deckFactory;
+        private readonly OutputService _outputService;
         private readonly PlayUtil _playUtil;
+        private readonly IRandomService _randomService;
 
         private readonly string ExitString = "x1234";
 
-        public ProgramViewmodel(OutputService outputService, 
+        public ProgramViewmodel(OutputService outputService,
             IRandomService randomService,
             IDeckFactory deckFactory,
             PlayUtil playUtil)
@@ -30,6 +30,8 @@ namespace CardGamePeer
             _deckFactory = deckFactory;
             _playUtil = playUtil;
         }
+
+        public bool ExitRequested { get; set; }
 
         private string FormatForConsole(string s)
         {
@@ -41,24 +43,21 @@ namespace CardGamePeer
             return _outputService.WriteLine(FormatForConsole(message));
         }
 
-        private Task<string> Prompt(string message)
+        private async Task<T?> Prompt<T>(string message, Func<string, ConvertResult<T>> convert, Func<T, bool> validate)
+            where T : struct
         {
-            return _outputService.Prompt(FormatForConsole(message));
-        }
-
-        private async Task<T?> Prompt<T>(string message, Func<string, T?> convert, Func<T, bool> validate,
-            string exitCode) where T : struct
-        {
-            T? converted;
+            ConvertResult<T> converted;
+            bool doContinue;
             do
             {
                 var stringValue = await _outputService.Prompt(FormatForConsole(message));
-                if (stringValue.ToLower().Trim() == exitCode.ToLower().Trim()) return null;
                 converted = convert(stringValue);
-            } while (converted != null &&
-                     !validate(converted.Value));
+                var isValid = converted.Result.HasValue && (validate?.Invoke(converted.Result.Value) ?? true);
+                doContinue = !converted.StopEarly &&
+                             !isValid;
+            } while (doContinue);
 
-            return converted;
+            return converted.Result;
         }
 
         public async Task Run()
@@ -71,15 +70,20 @@ namespace CardGamePeer
             {
                 new Player(player1Id, player1Id.ToString().Substring(0, 4)),
                 new Player(player2Id, player2Id.ToString().Substring(0, 4))
-            }.ToDictionary(p=>p.Id);
-            var deck = _deckFactory.Create().ToDictionary(c=>c.Id);
-            var game = new Game(Guid.NewGuid(), players.Select(p=>p.Key), _randomService, deck.Keys);
+            }.ToDictionary(p => p.Id);
+            var deck = _deckFactory.Create().ToDictionary(c => c.Id);
+            var game = new Game(Guid.NewGuid(), players.Select(p => p.Key), _randomService, deck.Keys);
 
             await WriteLine($"Starting Game: {game.Id}");
 
-            CardValue GetCardValue(Guid cardId) => deck[cardId].Value;
+            CardValue GetCardValue(Guid cardId)
+            {
+                return deck[cardId].Value;
+            }
+
             foreach (var round in game.Start(player2Id, GetCardValue))
             {
+                if (ExitRequested) break;
                 foreach (var turn in round.Start(GetCardValue))
                 {
                     await _outputService.WriteLine("");
@@ -87,53 +91,52 @@ namespace CardGamePeer
                     var hand = round.GetCurrentPlayerHand();
                     var previous = deck[hand.Previous];
                     var drawn = deck[hand.Drawn.Value];
-                    string input;
-                    do
-                    {
-                        await WriteLine($"Enter the number of the card you wish to play. Or {ExitString} to quit.");
-                        input = await Prompt(
-                            $"Hand {previous.TypeName}({previous.Value.GetHashCode()}) & {drawn.Value}({drawn.Value.GetHashCode()}).");
-                    } while (!ushort.TryParse(input, out var us) ||
-                             (CardValue) us != previous.Value &&
-                             (CardValue) us != drawn.Value &&
-                             input.ToLower().Trim() != ExitString);
 
-                    if (input.ToLower().Trim() == ExitString) break;
-                    var cardValue = (CardValue) ushort.Parse(input);
+                    await WriteLine($"Enter the number of the card you wish to play. Or {ExitString} to quit.");
+                    var x = await Prompt(
+                        $"Hand {previous.TypeName}({previous.Value.GetHashCode()}) & {drawn.Value}({drawn.Value.GetHashCode()}).",
+                        s =>
+                        {
+                            var stopEarly = ExitRequested = Sanitize(s) == ExitString;
+
+                            var result = ushort.TryParse(s, out var uval) ? (CardValue?) uval : null;
+                            return new ConvertResult<CardValue>(result, stopEarly);
+                        }, v => v == previous.Value || v == drawn.Value);
+                    if (ExitRequested) break;
+                    var cardValue = x.Value;
                     Guid? targetPlayer;
                     if (_playUtil.RequiresTargetPlayerToPlay(cardValue))
-                    {
                         targetPlayer = await GetTargetPlayer(round, players);
-                    }
                     else
-                    {
                         targetPlayer = null;
-                    }
 
                     CardValue? guessedCardvalue;
                     if (_playUtil.RequiresGuessedCardToPlay(cardValue))
-                    {
                         guessedCardvalue = await GetGuessedCardValue();
-                    }
                     else
-                    {
                         guessedCardvalue = null;
-                    }
 
                     if (!ExitRequested)
                     {
-                        var targetCardValue =_playUtil.RequiresTargetHandToPlay(cardValue) ? deck[round.RevealHand(targetPlayer.Value)].Value : (CardValue?)null;
+                        var targetCardValue = _playUtil.RequiresTargetHandToPlay(cardValue)
+                            ? deck[round.RevealHand(targetPlayer.Value)].Value
+                            : (CardValue?) null;
                         var playCard = deck[hand.First(c => deck[c].Value == cardValue)];
-                        _playUtil.Play(turn.CurrentPlayerId, playCard, turn, round, previous.Value, drawn.Value, targetPlayer, guessedCardvalue,targetCardValue);
+                        _playUtil.Play(turn.CurrentPlayerId, playCard, turn, round, previous.Value, drawn.Value,
+                            targetPlayer, guessedCardvalue, targetCardValue);
                     }
                 }
 
                 var winner = round.GetWinningPlayerId(GetCardValue);
                 if (winner != null)
-                {
-                    await WriteLine($"Round winner {GetPlayerDisplayName(game.Players.First(p => p == winner), players)}");
-                }
+                    await WriteLine(
+                        $"Round winner {GetPlayerDisplayName(game.Players.First(p => p == winner), players)}");
             }
+        }
+
+        private string Sanitize(string input)
+        {
+            return input.Trim().ToLower();
         }
 
         private async Task<Guid?> GetTargetPlayer(Round round, Dictionary<Guid, Player> players)
@@ -146,12 +149,20 @@ namespace CardGamePeer
             var messages = new[] {"Players:"}.Concat(playersList);
             var message = string.Join("\n", messages);
 
-            var targetIndex = await Prompt(message, i => ushort.TryParse(i, out var u) ? u : (ushort?) null,
-                u => u < remainingPlayers.Length, ExitString);
-            if (targetIndex == null){
+            var targetIndex = await Prompt(message,
+                i =>
+                {
+                    var stopEarly = ExitRequested = Sanitize(i) == ExitString;
+                    var result = ushort.TryParse(i, out var u) ? (ushort?) u : null;
+                    return new ConvertResult<ushort>(result, stopEarly);
+                },
+                u => u < remainingPlayers.Length);
+            if (targetIndex == null)
+            {
                 ExitRequested = true;
                 return null;
             }
+
             targetPlayer = remainingPlayers[targetIndex.Value];
             return targetPlayer;
         }
@@ -167,18 +178,22 @@ namespace CardGamePeer
             var message = string.Join("\n", messages);
 
             var targetValue = await Prompt(message,
-                i => Enum.TryParse(typeof(CardValue), i, out var u) ? (CardValue?) u : null,
-                u => values.Contains(u), ExitString);
+                i =>
+                {
+                    var stopEarly = ExitRequested = Sanitize(i) == ExitString;
+                    var result = Enum.TryParse(typeof(CardValue), i, out var u) ? (CardValue?) u : null;
+                    return new ConvertResult<CardValue>(result, stopEarly);
+                },
+                u => values.Contains(u));
             if (targetValue == null)
             {
                 ExitRequested = true;
                 return null;
             }
+
             guessedCardvalue = targetValue.Value;
             return guessedCardvalue;
         }
-
-        public bool ExitRequested { get; set; }
 
         private string GetPlayerDisplayName(Turn turn, Dictionary<Guid, Player> players)
         {
@@ -188,6 +203,18 @@ namespace CardGamePeer
         private string GetPlayerDisplayName(Guid playerId, Dictionary<Guid, Player> players)
         {
             return players[playerId].DisplayName;
+        }
+
+        public class ConvertResult<T> where T : struct
+        {
+            public ConvertResult(T? result, bool stopEarly)
+            {
+                Result = result;
+                StopEarly = stopEarly;
+            }
+
+            public T? Result { get; }
+            public bool StopEarly { get; }
         }
     }
 }
