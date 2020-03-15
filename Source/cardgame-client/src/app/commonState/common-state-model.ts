2ec5a33b2554897ecc3918bfe886c042664829a8
David Squire <squiredd@gmail.com>
2020-03-14 18:39:08 -0700

    enhanced common state

--- Source/cardgame-client/src/app/commonState/common-state-model.ts
+++ Source/cardgame-client/src/app/commonState/common-state-model.ts
@@ -6,16 +6,45 @@ export class CommonStateModel {
     private readonly stateChangedObservable: Observable<ICommonStateChanged>;
     public readonly StateId: Observable<string>;
     public readonly DrawCount: Observable<number>;
+    public readonly PlayerIds: Observable<string[]>;
+    public readonly Discard: Observable<ICard[]>;
+    public readonly CurrentPlayerId: Observable<string>;
+    public readonly PlayersInRound: Observable<string[]>;
     constructor(private connection: IOpenConnection) {
         this.stateChangedObservable = connection.register<ICommonStateChanged>('changed');
 
-        this.StateId = this.stateChangedObservable.pipe(
-            property(m => m.StateId)
-        );
+        this.StateId = this
+            .stateChangedObservable
+            .pipe(
+                property(m => m.StateId)
+            );
 
-        this.DrawCount = this.stateChangedObservable.pipe(
-            property(m => m.DrawCount)
-        );
+        this.DrawCount = this
+            .stateChangedObservable
+            .pipe(
+                property(m => m.DrawCount)
+            );
+
+        this.PlayerIds = this
+            .stateChangedObservable
+            .pipe(
+                property(m => m.PlayerIds)
+            );
+        this.Discard = this
+            .stateChangedObservable
+            .pipe(
+                property(m => m.Discard)
+            );
+        this.CurrentPlayerId = this
+            .stateChangedObservable
+            .pipe(
+                property(m => m.CurrentPlayerId)
+            );
+        this.PlayersInRound = this
+            .stateChangedObservable
+            .pipe(
+                property(m => m.PlayersInRound)
+            );
      }
 
     onCommonStateChanged(onCommonStateChanged: ICommonStateChanged) {
@@ -27,5 +56,13 @@ export class CommonStateModel {
 export interface ICommonStateChanged {
     readonly StateId: string;
     readonly DrawCount: number;
+    readonly PlayerIds: string[];
+    readonly Discard: ICard[];
+    readonly CurrentPlayerId: string;
+    readonly PlayersInRound: string[];
     [key: string]: any;
+}
+
+export interface ICard {
+    readonly Id: string;
 }
