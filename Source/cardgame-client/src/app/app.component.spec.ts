f1149b519b8d5441b2d0916d19e9be9dcc2201c9
David Squire <squiredd@gmail.com>
2020-03-18 23:33:25 -0700

    fix app component tests

--- Source/cardgame-client/src/app/app.component.spec.ts
+++ Source/cardgame-client/src/app/app.component.spec.ts
@@ -1,30 +1,26 @@
-import { TestBed, async } from '@angular/core/testing';
+import { Spectator, createComponentFactory } from '@ngneat/spectator';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { GameBoardComponent } from './gameBoard/game-board/game-board.component';
+import { MockComponent } from 'ng-mocks';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent,
-        GameBoardComponent
-      ],
-    }).compileComponents();
-  }));
+  let spectator: Spectator<AppComponent>;
+  const createComponent = createComponentFactory({
+    component: AppComponent,
+    declarations: [MockComponent(GameBoardComponent)],
+    imports: [RouterTestingModule]
+  });
+
+  beforeEach(() => spectator = createComponent());
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = spectator.component;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'cardgame-client'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = spectator.component;
     expect(app.title).toEqual('cardgame-client');
   });
 });
