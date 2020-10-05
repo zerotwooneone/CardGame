ac9e5c9f8d70c803b6672d4d6446468a5e7b1024
David Squire <squiredd@gmail.com>
2020-10-04 22:08:04 -0700

    clean up connect

--- Source/cardgame-client/src/app/app.component.ts
+++ Source/cardgame-client/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonStateFactoryService } from './commonState/common-state-factory.service';
 import { ClientFactoryService } from './client/client-factory.service';
-import { first, timeout } from 'rxjs/operators';
 import { ClientRouterService } from './client/client-router.service';
 import { FormControl, Validators } from '@angular/forms';
-import { GameClientFactoryService } from './game/game-client-factory.service';
 import { GameModelFactoryService } from './game/game-model-factory.service';
 import { CurrentPlayerModelFactoryService } from './currentPlayer/current-player-model-factory.service';
 import { GameModel } from './game/game-model';
-import { CommonStateModel } from './commonState/common-state-model';
 import { CurrentPlayerModel } from './currentPlayer/current-player-model';
 
 @Component({
@@ -26,7 +23,6 @@ export class AppComponent implements OnInit {
   constructor(private readonly clientFactory: ClientFactoryService,
     private readonly clientRouter: ClientRouterService,
     private readonly commonStateFactory: CommonStateFactoryService,
-    private readonly gameClientFactory: GameClientFactoryService,
     private readonly gameModelFactory: GameModelFactoryService,
     private readonly currentPlayerModelFactory: CurrentPlayerModelFactoryService) { }
 
@@ -39,10 +35,9 @@ export class AppComponent implements OnInit {
   async connect(gameId: string, playerId: string) {
     this.gameIdX = gameId;
     try {
+      // we call this to open the signalR connection
       const c = await this.clientFactory.Create({ GameId: gameId });
-      // console.log('created client');
-      // const state = await c.State.pipe(first(), timeout(100)).toPromise();
-      // console.log(state);
+
       const commonState = await this.commonStateFactory.get(gameId);
       this.gameModel = await this.gameModelFactory.create(commonState, gameId);
 
