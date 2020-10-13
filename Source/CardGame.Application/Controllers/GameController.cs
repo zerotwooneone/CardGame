99942cf12639d9eab404ae1884427530ba8430eb
David Squire <squiredd@gmail.com>
2020-10-12 23:54:23 -0700

    changed how action result is returned

--- Source/CardGame.Application/Controllers/GameController.cs
+++ Source/CardGame.Application/Controllers/GameController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using CardGame.Application.DTO;
 using CardGame.CommonModel.Bus;
 using CardGame.Domain.Abstractions.Game;
 using CardGame.Utils.Abstractions.Bus;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PlayRequest = CardGame.Application.DTO.PlayRequest;
@@ -18,16 +20,19 @@ namespace CardGame.Application.Controllers
         private readonly IGameDal _gameDal;
         private readonly IGameConverter _gameConverter;
         private readonly IBus _bus;
+        private readonly IHostingEnvironment _hostingEnvironment;
 
         public GameController(ILogger<GameController> logger,
             IGameDal gameDal,
             IGameConverter gameConverter,
-            IBus bus)
+            IBus bus,
+            IHostingEnvironment hostingEnvironment)
         {
             _logger = logger;
             _gameDal = gameDal;
             _gameConverter = gameConverter;
             _bus = bus;
+            _hostingEnvironment = hostingEnvironment;
         }
 
         [HttpGet]
@@ -42,9 +47,10 @@ namespace CardGame.Application.Controllers
         [Route("{gameId}/Play")]
         public async Task<ActionResult> Post(Guid gameId, PlayRequest request)
         {
+            var correlationId = Guid.NewGuid();
             var serviceRequest = new Domain.Abstractions.Game.PlayRequest
             {
-                CorrelationId = Guid.NewGuid(),
+                CorrelationId = correlationId,
                 GameId = gameId,
                 CardStrength = request.CardStrength,
                 CardVarient = request.CardVariant,
@@ -52,15 +58,42 @@ namespace CardGame.Application.Controllers
                 GuessValue = request.GuessValue,
                 TargetId = request.TargetId
             };
-            
+
+            var cancellationToken = GetRequestCancellationToken();
+
             var response =
-                await _bus.Request<Domain.Abstractions.Game.PlayRequest, CardPlayed>("CardGame.Domain.Abstractions.Game.IGameService:Play", serviceRequest);
-            if (string.IsNullOrWhiteSpace(response.ErrorMessage))
+                await _bus.Request<Domain.Abstractions.Game.PlayRequest, CardPlayed>(
+                    "CardGame.Domain.Abstractions.Game.IGameService:Play", 
+                    serviceRequest, 
+                    cancellationToken);
+            if (_hostingEnvironment.IsDevelopment())
             {
-                return new JsonResult(response);
+                var result = new JsonResult(response);
+                return result;
             }
+            if (cancellationToken.IsCancellationRequested || string.IsNullOrWhiteSpace(response.ErrorMessage))
+            {
+                var result = Accepted(new {CorrelationId = correlationId});
+                result.Location = GetGameUrl(gameId);
+                return result;
+            }
+            
 
             return BadRequest(response.ErrorMessage);
+        }
+
+        private string GetGameUrl(Guid gameId)
+        {
+            return $"~/game/{gameId}";
+        }
+
+        private CancellationToken GetRequestCancellationToken()
+        {
+            CancellationTokenSource cts = _hostingEnvironment.IsDevelopment()
+                ? new CancellationTokenSource()
+                : new CancellationTokenSource(TimeSpan.FromSeconds(5));
+            var cancellationToken = cts.Token;
+            return cancellationToken;
         }
 
         [HttpGet]
