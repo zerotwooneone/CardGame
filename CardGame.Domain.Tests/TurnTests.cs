fb751ce6a444fb81deda9e94f1d669d5bb8386c4
zero <squiredd@gmail.com>
2024-11-23 13:04:17 -0800

    add domain tests project

---
+++ CardGame.Domain.Tests/TurnTests.cs
@@ -0,0 +1,16 @@
+namespace CardGame.Domain.Tests;
+
+public class TurnTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        //_turnRepository = new TestRepository();
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
