3c9a53c8806f2fb8fe434ba4a63265a28e9bea10
zero <squiredd@gmail.com>
2025-04-26 18:15:46 -0700

    upgrade to angular 19 and fix some things

--- CardGame.Web/ClientApp/src/app/app.module.ts
+++ CardGame.Web/ClientApp/src/app/app.module.ts
@@ -1,5 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {APP_ID, NgModule} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
@@ -9,6 +8,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import {CommonModule} from "@angular/common";
 
 @NgModule({ declarations: [
         AppComponent,
@@ -17,11 +17,20 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
         CounterComponent,
         FetchDataComponent
     ],
-    bootstrap: [AppComponent], imports: [BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+
+    bootstrap: [AppComponent],
+  imports: [
         FormsModule,
         RouterModule.forRoot([
             { path: '', component: HomeComponent, pathMatch: 'full' },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
-        ])], providers: [provideHttpClient(withInterceptorsFromDi())] })
+        ]),
+    CommonModule],
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: APP_ID,  useValue: 'ng-cli-universal' }
+  ],
+  },
+  )
 export class AppModule { }
