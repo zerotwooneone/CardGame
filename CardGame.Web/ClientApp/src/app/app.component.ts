7fcf70057d948ff9ebccd4cc74998475150a3f9d
zero <squiredd@gmail.com>
2025-04-26 20:02:20 -0700

    replace angular client with cli generated v19

--- CardGame.Web/ClientApp/src/app/app.component.ts
+++ CardGame.Web/ClientApp/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    standalone: false
+  selector: 'cgc-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'app';
+  title = 'CardGameClient';
 }
